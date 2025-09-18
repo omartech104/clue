29c8b2473005630b34f843ead8aad7a3cb08c9eb
Omar Mohamed <hssnomr2022@gmail.com>
2025-09-19 00:06:05 +0300

    adding small changes

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ while running:
         print("This program only runs on Linux")
     
     command = input("Enter the command you want to know about: ").lower()
-    if command:
+    def recive_info_command(command):
         url = f"https://cheat.sh/{command}?QT"
         try:
             response = requests.get(url)
             if response.status_code == 200:
                 print(response.text)
-                input("Press Enter to continue...")
             else:
                 print("Error fetching cheat sheet.")
         except Exception as e:
             print(f"Request failed: {e}")
+    recive_info_command(command)
+    input("Press Enter to continue...")
